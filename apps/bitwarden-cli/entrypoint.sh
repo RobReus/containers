0732207c45db8c2a7e03b209e3be8358104ae0e1
Rob Reus <rob@devrobs.nl>
2024-07-15 19:09:26 +0200

    Fixing bitwarden-cli

--- apps/bitwarden-cli/entrypoint.sh
+++ apps/bitwarden-cli/entrypoint.sh
@@ -2,11 +2,20 @@
 
 set -e
 
+if [[ ! -z "${BW_APPID}" ]]; then
+  if [[ -z "${BITWARDENCLI_APPDATA_DIR}" ]]; then
+    echo "BITWARDENCLI_APPDATA_DIR is not set, must be set when using BW_APPID."
+    exit 1
+  fi
+  mkdir -p ${BITWARDENCLI_APPDATA_DIR}
+  echo "{\"appId\": \"${BW_APPID}\"}" > ${BITWARDENCLI_APPDATA_DIR}/data.json
+fi
+
 bw config server ${BW_HOST}
 
 export BW_SESSION=$(bw login ${BW_USER} --passwordenv BW_PASSWORD --raw)
 
 bw unlock --check
 
-echo 'Running `bw server` on port 8087'
+echo 'Running `bw serve` on port 8087'
 bw serve --hostname 0.0.0.0 #--disable-origin-protection
