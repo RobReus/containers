d37298a0aa118d5675c96c24aaf16d0ff4decdcb
Rob Reus <rob@devrobs.nl>
2024-09-09 10:22:03 +0200

    Fixing papermc

--- apps/papermc/ci/latest.sh
+++ apps/papermc/ci/latest.sh
@@ -3,6 +3,4 @@ set -x
 VERSION=$(curl -sX GET https://papermc.io/api/v2/projects/paper | jq -r '.versions[-1]')
 BUILD=$(curl -sX GET https://papermc.io/api/v2/projects/paper/versions/${VERSION} | jq -r '.builds[-1]')
 FULL="${VERSION}-${BUILD}"
-printf "%s\n" "${VERSION}-${BUILD}"
-printf "%s\n" "${FULL#*-}"
-printf "%s\n" "${FULL%-*}"
+printf "%s" "${VERSION}-${BUILD}"
